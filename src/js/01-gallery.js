af2f39feb16782fb2b77911abaa0ad664dacb541
Jarek Dzwolak <jaroslawdzwolak@gmail.com>
2023-03-27 21:22:49 +0200

    Frist coomit

---
+++ src/js/01-gallery.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+console.log(galleryItems);
+
+const MyGallery = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const imageItem = document.createElement("li");
+  MyGallery.append(imageItem);
+
+  imageItem.insertAdjacentHTML(
+    "afterend",
+    `<a class="gallery__link" href="${image.original}">
+    <img
+      class="gallery__image"
+      src="${image.preview}"
+      data-source="${image.original}"
+      alt="${image.description}"
+    />
+  </a>`
+  );
+});
